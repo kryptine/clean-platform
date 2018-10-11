0098da0f5912f37fccea79b320d4762ed9999842
Mart Lubbers <mart@martlubbers.net>
2018-10-11 12:11:12 +0200

    better packages

--- mkpkgs.sh
+++ mkpkgs.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 #linux
-mkdir CleanSerial-linux64
+mkdir CleanSerial
 rm -rf "./Clean System Files/*"
 make -B 'Clean System Files/ctty.o'
-mv 'Clean System Files' CleanSerial-linux64
-cp TTY.[id]cl iTasksTTY.[id]cl POSIX/Platform.[id]cl CleanSerial-linux64
-tar -czvf CleanSerial-linux64.tar.gz CleanSerial-linux64
-rm -r CleanSerial-linux64
+mv 'Clean System Files' CleanSerial
+cp TTY.[id]cl iTasksTTY.[id]cl POSIX/Platform.[id]cl CleanSerial
+tar -czvf CleanSerial-linux64.tar.gz CleanSerial
+rm -r CleanSerial
 
 #windows64
-mkdir CleanSerial-win64
+mkdir CleanSerial
 rm -rf "./Clean System Files/*"
 DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc make -B 'Clean System Files/ctty.o'
-mv 'Clean System Files' CleanSerial-win64
-cp TTY.[id]cl iTasksTTY.[id]cl Windows/Platform.[id]cl CleanSerial-win64
-zip -rv CleanSerial-win64.zip CleanSerial-win64
-rm -r CleanSerial-win64
+mv 'Clean System Files' CleanSerial
+cp TTY.[id]cl iTasksTTY.[id]cl Windows/Platform.[id]cl CleanSerial
+zip -rv CleanSerial-win64.zip CleanSerial
+rm -r CleanSerial
 
 #windows32
-mkdir CleanSerial-win32
+mkdir CleanSerial
 rm -rf "./Clean System Files/*"
 DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc-win32 make -B 'Clean System Files/ctty.o'
-mv 'Clean System Files' CleanSerial-win32
-cp TTY.[id]cl iTasksTTY.[id]cl Windows/Platform.[id]cl CleanSerial-win32
-zip -rv CleanSerial-win32.zip CleanSerial-win32
-rm -r CleanSerial-win32
+mv 'Clean System Files' CleanSerial
+cp TTY.[id]cl iTasksTTY.[id]cl Windows/Platform.[id]cl CleanSerial
+zip -rv CleanSerial-win32.zip CleanSerial
+rm -r CleanSerial
