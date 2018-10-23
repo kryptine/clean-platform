f34b3f360a6d5c81b0515f12998e55c88070ceef
Mart Lubbers <mart@martlubbers.net>
2018-10-23 08:16:11 +0200

    fix ci for windows

--- mkpkgs.sh
+++ mkpkgs.sh
@@ -5,15 +5,16 @@ trap "rm -rf CleanSerial" EXIT
 mkpkg() {
 	rm -rf CleanSerial
 	mkdir -p CleanSerial
-	make $1 -C src clean all
+	make -B $1 -C src clean all
 	cp -Rv src/"Clean System Files" CleanSerial
 	cp -v src/{TTY,iTasksTTY}.[id]cl src/$2/Platform.[id]cl CleanSerial
+	file CleanSerial/*/*.o
 	$3 CleanSerial-$4 CleanSerial
 }
 
 #linux
 mkpkg "" POSIX "tar -czvf" linux64.tar.gz
 #windows64
-mkpkg "DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc" Windows "zip -rv" win64.zip
+mkpkg "DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc CFLAGS=-m64" Windows "zip -rv" win64.zip
 #windows32
-mkpkg "DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc-win32" Windows "zip -rv" win32.zip
+mkpkg "DETECTED_OS=Windows CC=x86_64-w64-mingw32-gcc-win32 CFLAGS=-m32" Windows "zip -rv" win32.zip
