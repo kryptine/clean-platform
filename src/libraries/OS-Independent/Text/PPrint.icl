kryptine/clean-platform

commit c1846b22623daf8e6c359213c62eb4aac525d563

Foldable default

src/libraries/OS-Independent/Text/PPrint.icl
@@ -3,7 +3,6 @@ implementation module Text.PPrint
 import StdEnv
 import Data.List
 import Data.Maybe
-from Data.Foldable import class Foldable(foldr1)
 
 /*
  * PPrint
@@ -100,7 +99,7 @@ vcat            = fold (<$$>)
 
 fold :: (Doc Doc ->Doc) ![Doc] -> Doc
 fold f []       = empty
-fold f ds       = foldr1 f ds
+fold f [d:ds]   = foldr f d ds
 
 (<->) infixr 6 :: Doc Doc -> Doc
 (<->) x y         = beside x y

src/libraries/OS-Independent/Clean/Types/Tree.icl
@@ -1,11 +1,9 @@
 implementation module Clean.Types.Tree
 
 import StdBool
-from StdFunc import o
+import StdInt
+import StdFunctions
 import StdOrdList
-import StdOverloaded
-import StdString
-import StdTuple
 
 import Clean.Types
 import Clean.Types.Unify

src/libraries/OS-Independent/Clean/Types/Util.icl
@@ -2,7 +2,9 @@ implementation module Clean.Types.Util
 
 import StdArray
 import StdBool
-from StdFunc import flip, id, o
+import StdChar
+import StdFunctions
+import StdInt
 import StdMisc
 import StdOrdList
 import StdString
@@ -11,9 +13,8 @@ import StdTuple
 import Clean.Types
 import Control.Applicative
 import Control.Monad
-from Data.Func import $
+import Data.Func
 import Data.Functor
-import Data.GenEq
 import Data.List
 from Data.Map import :: Map(..), get
 import Data.Maybe

src/libraries/OS-Independent/Data/Foldable.dcl
@@ -5,10 +5,13 @@ from Control.Applicative import class pure(pure), class <*>, class Applicative,
 from Control.Monad import class Monad(bind), >>=, class MonadPlus(mzero,mplus)
 from Data.Functor import class Functor
 from Data.Monoid import class Monoid, class Semigroup
-from Data.Maybe import :: Maybe
+from Data.Maybe import :: Maybe(..)
 from StdOverloaded import class +, class one, class *, class zero, class <, class ==
+from Data.List import instance Foldable []
 from StdClass import class Ord
-from StdFunc import flip
+from StdMisc import abort
+from Data.Monoid import :: Endo(Endo), :: Dual(Dual), class Monoid(mempty), class Semigroup(mappend), appEndo, getDual
+from StdFunc import flip, id
 
 /**
  * Ported from Haskell's Data.Foldable by Jurriën Stutterheim 15-08-2014
@@ -40,50 +43,75 @@ class Foldable t where
 	/**
 	 * Combine the elements of a structure using a monoid.
 	 */
-    fold :: !(t m) -> m | Monoid m
+	fold :: !(t m) -> m | Monoid m
+	fold a = foldMap id a
 
 	/**
 	 * Map each element of the structure to a monoid, and combine the results.
 	 */
-    foldMap :: (a -> m) !(t a) -> m | Monoid m
+	foldMap :: (a -> m) !(t a) -> m | Monoid m
+	foldMap f t = foldr (\x->mappend (f x)) mempty t
 
 	/**
 	 * Right-associative fold of a structure.
 	 * `foldr f z = 'StdList'.{{foldr}} f z {{o}} {{toList}}`
 	 */
-    foldr :: (a .b -> .b) .b !(t a) -> .b
+	foldr :: (a .b -> .b) .b !(t a) -> .b
+	foldr f z t = appEndo (foldMap (Endo f) t) z
 
 	/**
 	 * Right-associative fold of a structure, but with strict application of
 	 * the operator.
 	 */
-    foldr` :: (a .b -> .b) !.b !(t a) -> .b
+	foldr` :: (a .b -> .b) !.b !(t a) -> .b
+	foldr` f z0 xs = foldl (\k x z->sapp k (f x z)) (\x.x) xs z0
+	where
+		sapp :: .(.a -> .b) !.a -> .b
+		sapp f x = f x
 
 	/**
 	 * Left-associative fold of a structure.
 	 * `foldl f z = 'StdList'.{{foldl}} f z o {{toList}}`
 	 */
-    foldl :: (.b -> .(a -> .b)) .b !(t a) -> .b
+	foldl :: (.b -> .(a -> .b)) .b !(t a) -> .b
+	foldl f z t = appEndo (getDual (foldMap (\x->Dual (Endo (flip f x))) t)) z
 
 	/**
 	 * Left-associative fold of a structure, but with strict application of the
 	 * operator.
 	 */
-    foldl` :: (.b -> .(a -> .b)) !.b !(t a) -> .b
+	foldl` :: (.b -> .(a -> .b)) !.b !(t a) -> .b
+	foldl` f z0 xs = foldr (\x k z->sapp k (f z x)) (\x.x) xs z0
+	where
+		sapp :: .(.a -> .b) !.a -> .b
+		sapp f x = f x
 
 	/**
 	 * A variant of {{foldr}} that has no base case, and thus may only be
 	 * applied to non-empty structures.
 	 * `foldr1 f = 'Data.List'.{{foldr1}} f o {{toList}}`
 	 */
-    foldr1 :: !(a a -> a) !(t a) -> a
-
+	foldr1 :: !(a a -> a) !(t a) -> a
+	foldr1 f x = case foldr mf Nothing x of
+		Nothing = abort "foldr1: empty structure\n"
+		Just x = x
+	where
+		mf x m = Just (case m of
+			Nothing -> x
+			Just y  -> f x y)
 	/**
 	 * A variant of {{foldl}} that has no base case, and thus may only be
 	 * applied to non-empty structures.
 	 * `foldl1 f = 'Data.List'.{{foldl1}} f o {{toList}}`
 	 */
-    foldl1 :: !(a a -> a) !(t a) -> a
+	foldl1 :: !(a a -> a) !(t a) -> a
+	foldl1 f x = case foldl mf Nothing x of
+		Nothing = abort "foldl1: empty structure\n"
+		Just x = x
+	where
+		mf m y = Just (case m of
+			Nothing -> y
+			Just x  -> f x y)
 
 // TODO Cleanify
 //instance Ix i => Foldable (Array i)

src/libraries/OS-Independent/Data/Graph/Inductive/Basic.icl
@@ -6,6 +6,7 @@ import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.Internal.Thread
 
 import Data.List
+import StdList
 from StdFunc import o
 import StdTuple
 

src/libraries/OS-Independent/Data/Graph/Inductive/Graph.icl
@@ -9,7 +9,7 @@ import qualified Data.IntSet
 import qualified Data.List
 //import           Data.Maybe    (fromMaybe, isJust)
 //import Data.Monoid (mappend)
-import StdBool, StdTuple, StdFunc, StdMisc, StdEnum, StdString, StdOverloaded, StdClass
+import StdList, StdBool, StdTuple, StdFunc, StdMisc, StdEnum, StdString, StdOverloaded, StdClass
 import Data.List
 import Data.Maybe
 import Data.Functor

src/libraries/OS-Independent/Data/Graph/Inductive/Internal/RootPath.icl
@@ -4,7 +4,7 @@ implementation module Data.Graph.Inductive.Internal.RootPath
 
 import Data.Graph.Inductive.Graph
 import Data.List
-import StdFunc, StdTuple
+import StdFunc, StdTuple, StdInt
 
 :: LRTree a :== [LPath a]
 :: RTree    :== [Path]

src/libraries/OS-Independent/Data/Graph/Inductive/NodeMap.icl
@@ -34,7 +34,7 @@ fromGraph :: (g a b) -> NodeMap a | Ord a & Graph g
 fromGraph g =
     let ns = 'Data.Graph.Inductive.Graph'.labNodes g
         aux (n, a) (m`, k`) = ('Data.Map'.put a n m`, max n k`)
-        (m, k) = 'Data.List'.foldr aux ('Data.Map'.newMap, 0) ns
+        (m, k) = foldr aux ('Data.Map'.newMap, 0) ns
     in {NodeMap | map = m, key = k+1 }
 
 // | Generate a labelled node from the given label.  Will return the same node

commit bca628f104b11befa7ff6bbef0f76704b5883b27

Fix bug in Text.PPrint introduced in c1846b22

src/libraries/OS-Independent/Text/PPrint.icl
@@ -1,6 +1,7 @@
 implementation module Text.PPrint
 
 import StdEnv
+from Data.Foldable import class Foldable(foldr1)
 import Data.List
 import Data.Maybe
 
@@ -99,7 +100,7 @@ vcat            = fold (<$$>)
 
 fold :: (Doc Doc ->Doc) ![Doc] -> Doc
 fold f []       = empty
-fold f [d:ds]   = foldr f d ds
+fold f ds       = foldr1 f ds
 
 (<->) infixr 6 :: Doc Doc -> Doc
 (<->) x y         = beside x y